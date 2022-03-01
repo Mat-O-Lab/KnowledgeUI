aaa78dba725ccdf67ac7b7c8b7855d936ac72545
Ilias Deligiannis <iliass.deligiannis@gmail.com>
2022-03-01 14:38:01 +0100

    Generate dictionary to map class names to prefixes

--- app/home/query.py
+++ app/home/query.py
@@ -20,6 +20,8 @@ else:
 #We just need to change the name with the actual database to be useds
 sparql = SPARQLWrapper("https://dataconnect.bam.de/graph/lebedigital-emodul/query")
 
+classPrefixDic = dict()
+
 prefixes = [
             '<http://www.w3.org/2002/07/owl#>',
             '<https://www.materials.fraunhofer.de/ontologies/graph_designer#>',
@@ -46,6 +48,13 @@ def get_name_from_uri(uri):
 
     name = uri[i:] if k < i else uri[k:]
     return name
+
+def get_prefix_from_uri(uri):
+    i = uri.rfind('/') + 1
+    k = uri.rfind('#') + 1
+
+    pf = uri[:i] if k < i else uri[:k]
+    return pf
 
 def send_query(q1):
     sparql.setQuery(q1)
@@ -328,9 +337,7 @@ def continue_string_search(search):
     df = pd.DataFrame({'s': s, 'p': p, 'o': o})
     return df
 
-#Allows to get a list with all the occurencies in the DB
-#For the moment s, p ans o in the same list and doing that only once
-def initAutocompleteList():
+def initclassPrefixDic():
     results = send_query("""
     prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     prefix owl: <http://www.w3.org/2002/07/owl#>
@@ -339,10 +346,17 @@ def initAutocompleteList():
     WHERE {
        ?wanted a owl:Class .
     }
-""")
+    """)
 
-    l = list()
+    dc =dict()
     for s in results["results"]["bindings"]:
-        l.append(get_name_from_uri(s['wanted']['value']))
-    return l
+        dc[get_name_from_uri(s['wanted']['value'])] = get_prefix_from_uri(s['wanted']['value'])
+    return dc
     
+#Allows to get a list with all the occurencies in the DB
+#For the moment s, p ans o in the same list and doing that only once
+def initAutocompleteList():
+    
+    classPrefixDic = initclassPrefixDic()
+    l = list(classPrefixDic.keys())
+    return l
