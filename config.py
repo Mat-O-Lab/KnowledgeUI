6f338847cb17f94ccc1774e9ced86159e28b08c9
soudehMasoudian <soudeh.masoudian@gmail.com>
2022-07-21 13:22:24 +0200

    revert This reverts commit 0a97b139274abd573258be1c4b21d7e9ca7cd0c9.

--- config.py
+++ config.py
@@ -1,44 +1,15 @@
 import os
 
 class Config:
-    """
-    This class defines some gneral configs 
-
-    ....
-
-    Attributes
-    ----------
-    SECRET_KEY: str
-        get the private_key from environment variable
-    TEMPORAL_FOLDER: str
-        defines a temporary folders to use as tmp
-    TEMPLATES_AUTORELOAD: boolean
-        defines autoreload property 
-    SPARQL_ENDPOINT: str
-        get sparql endpoint
-    """
-
     SECRET_KEY = os.environ.get("SECRET_KEY") or "harder to guess string"
     TEMPORAL_FOLDER = os.environ.get("TEMPORAL_FOLDER") or "tmp"
     TEMPLATES_AUTORELOAD = True
     SPARQL_ENDPOINT = 'https://fuseki.matolab.org/alutrace/sparql'
 
 class DevelopmentConfig(Config):
-    """
-    Attributes
-    ----------
-    config: object
-    """
-
     DEBUG = True
 
 class ProductionConfig(Config):
-    """
-    Attributes
-    ----------
-    config: object
-    """
-
     DEBUG = False
 
 config = {
