aba2386d0041cdfcd29af668d02d5426f2ebb0b8
soudehMasoudian <soudeh.masoudian@gmail.com>
2022-07-21 13:12:56 +0200

    add docstring to py files

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, flash, request, jsonify, render_template
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from flask_cors import CORS
-
 from config import config
 
 config_name = os.environ.get("APP_MODE") or "development"
@@ -16,6 +15,16 @@ bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """ This is the main function which redirects here after lunching
+
+    It allows both post and get methods and initializes some general parameters
+    like logo and message once called
+
+    Parameters
+    ----------
+
+    """
+
     logo = './static/resources/MatOLab-Logo.svg'
     message = ''
     result = ''
@@ -26,10 +35,22 @@ def index():
         result=result
         )
 
+# TODO: add logo, message and result to a general function instead of defining in every function separately
+
 @app.route('/osparklis.html', methods=['GET'])
 def explore():
+    """ render explore tab in the main page
+    it only allows get methods
+
+    Parameters
+    
+    
+    """
+    
+    logo = './static/resources/MatOLab-Logo.svg'
     return render_template(
-        "osparklis.html"
+        "osparklis.html",
+        logo=logo
     )
 
 if __name__ == "__main__":
