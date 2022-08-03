015e8eb0d263fcf790581a0e1271a91b4503a428
Thomas Hanke <38633260+ThHanke@users.noreply.github.com>
2022-08-03 15:55:44 +0200

    Update config.py
    merged data endpoint gives weird results

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "harder to guess string"
     TEMPORAL_FOLDER = os.environ.get("TEMPORAL_FOLDER") or "tmp"
     TEMPLATES_AUTORELOAD = True
-    SPARQL_ENDPOINT = 'https://fuseki.matolab.org/mergeAllutraceBWMDWeb4GenMet/sparql'
+    SPARQL_ENDPOINT = 'https://fuseki.matolab.org/alutrace/sparql'
     SPARKLIS_OPTIONS = "&entity_lexicon_select=http%3A//www.w3.org/2000/01/rdf-schema%23label&concept_lexicons_select=http%3A//www.w3.org/2000/01/rdf-schema%23label&concept_tooltips_select=http%3A//www.w3.org/2000/01/rdf-schema%23comment"
 
 class DevelopmentConfig(Config):
