4acb6430328833ac714ce7b83b0aad56407d95f4
ghezal ahmad <ghezalahmad.zia@outlook.com>
2022-09-22 11:25:50 +0200

    Slamd File created

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def query():
         "predef.html",
         logo=logo
     )
-
+""" 
 @app.route('/predict', methods=['GET'])
 def predict():
     logo = './static/resources/MatOLab-Logo.svg'
@@ -79,6 +79,56 @@ def predict():
         "predict.html",
         logo=logo
     )
+"""
+
+@app.route('/predict, methods=['POST'])
+def model_process(dataset = dataset):
+    model = request.form.get('models')
+    target_df = request.form.getlist('targets')
+    feature_df = request.form.getlist('feature_df')
+    fixed_target_df = request.form.getlist('fixedtargets')
+    strategy = request.form.get('strategies')
+    #distance = request.form.get('initial_sample')
+    sigma = float(request.form.get('sigma_factor'))
+
+    #dataframe = loadDataset(dataset)
+
+    # --- This is the min_max of benchmarking ---------
+    min_or_max_target = {}
+    for t in target_df:
+        x = 'Rd_'+t
+        min_or_max_target[t]= request.form.get(x)
+
+    target_selected_number2 = {}
+    for t in target_df:
+        x = 'Nd_'+t
+        target_selected_number2[t]= int(request.form.get(x))
+    #---------------------------------
+    min_or_max_fixedtarget = {}
+    for t in fixed_target_df:
+        x = 'Rd1_'+t
+        min_or_max_fixedtarget[t]= request.form.get(x)
+
+
+    fixedtarget_selected_number2 = {}
+    for t in fixed_target_df:
+        x = 'Nd1_'+t
+        fixedtarget_selected_number2[t]= int(request.form.get(x))
+    # ------------------------------------------------
+
+    l = learn(dataframe, model, target_df, feature_df, fixed_target_df, strategy, sigma, target_selected_number2, fixedtarget_selected_number2, min_or_max_target, min_or_max_fixedtarget)
+    l.start_learning()
+    n = l.start_learning()
+
+    df_table = pd.DataFrame(n)
+    df_column = df_table.columns
+    #df_table2 = df_table1[1:]
+    print(df_column)
+    df_only_data = df_table
+
+
+
+    return render_template('scores.html', dataset=dataset, logo=logo, df_column=df_column,  df_only_data=df_only_data, n=n.to_html(index=False, classes='table table-striped table-hover table-responsive', escape=False))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
